53074c6e799d9cd8bfc4f5b5df4992f025ecce9b
Herman <goethesum@gmail.com>
2021-05-18 12:28:03 +0500

    added session management

--- webapp/cmd/web/main.go
+++ webapp/cmd/web/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"github.com/mrazen/webapp/pkg/config"
 	"github.com/mrazen/webapp/pkg/handlers"
 	"github.com/mrazen/webapp/pkg/render"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
